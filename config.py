1a9a8bd1b9af4a9f09b3868359db73db8eb963b7
Radheyojha09660 <ojha.radhey1995@gmail.com>
2026-01-08 19:27:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "8453743922:AAGMoccAImCgweSU9rULgSaDjaKg
 API_ID = int(os.environ.get("API_ID", "26704085"))
 API_HASH = os.environ.get("API_HASH", "f150646c78f09b4f88bef191a22539c0")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "7640198485").split(",")]
-LOG_CHANNEL = "3046463063"
+LOG_CHANNEL = "-1003508871162"
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "https://radhey-save-restrict-bot.onrender.com")
 # Rexbots
